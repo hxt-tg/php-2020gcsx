aa08009ddec6b41b6a60fe6e9ba2fe44bc0aa975
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2020-09-12 12:12:11 +0800

    init framework built: PHP-based.

---
+++ php-2020gcsx/common/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('APP_NAME', '2020GCSX_YNU');
+define('APP_ABSPATH', __DIR__ . '/../');
+define('DB_ADDR', '');
+define('DB_PORT', '3306');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+define('DB_PREFIX', '');
+
---
+++ php-2020gcsx/lib/city.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once '../common/dbengine.php';
+
+class City
+{
+
+}
